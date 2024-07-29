bebe56f24b0022e285525770540ef325c89b5663
Tushar Kutre <115899383+tusharkutre@users.noreply.github.com>
2024-07-29 11:50:42 +0530

    added java files of classes and objects

---
+++ Tripod.java
@@ -0,0 +1,16 @@
+//declared class
+class Tripod{
+	
+//Created non-static variable
+//String : null value
+//double or int : 0.0 value
+
+String brand;
+double price ;
+boolean isTrue;
+
+ public Tripod(){
+	 System.out.println("tripod created");
+ }
+
+}	
